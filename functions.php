3591b0e69e99dfe5dadd5f8694c7bcb8fe05f733
cream <lamar.mcmiller@gmail.com>
2018-03-16 22:45:56 -0400

    Cleaned up some uneeded files
    Refactored gulpfile for Wordpress theme development

--- functions.php
+++ functions.php
@@ -111,16 +111,14 @@ add_action( 'after_setup_theme', 'lm_content_width', 0 );
  */
 
 function lm_scripts() {
-	wp_enqueue_style( 'lm-style', get_stylesheet_uri() );
 
    wp_enqueue_style('lm-bs-css', get_template_directory_uri() . '/dist/css/bootstrap.min.css');
+
+	wp_enqueue_style( 'lm-style', get_stylesheet_uri() );
 
    wp_enqueue_style('lm-fa-css', get_template_directory_uri() . '/dist/css/font-awesome.min.css');
 
    wp_register_script('popper', '/src/js/popper.min.js'. false, '', true);
-
-	// wp_enqueue_script('lm-tether', get_template_directory_uri().'/src/js/tether.min.js',
-	// array(), '20180315', true);
 
    wp_enqueue_script('lm-jquery',
 	get_template_directory_uri().'/src/js/jquery.min.js',
@@ -136,6 +134,9 @@ function lm_scripts() {
 	'/src/js/nav-scroll.js', array('jquery'), '20180315', true);
 
 	wp_enqueue_script( 'lm-skip-link-focus-fix', get_template_directory_uri() . '/src/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'gulp-wordpress-javascript', get_template_directory_uri() . './dist/js/app.min.js', array(), '20180316', true );
+
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,42 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
 const plumber = require('gulp-plumber');
 const gutil = require('gulp-util');
 const jshint = require('gulp-jshint');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-require('es6-promise').polyfill();
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const rtlcss = require('gulp-rtlcss');
 const rename = require('gulp-rename');
 const imageMin = require('gulp-imagemin');
 const inject = require('gulp-inject');
 const cache = require('gulp-cache');
+const groupmq = require('gulp-group-css-media-queries');
+require('es6-promise').polyfill();
 
-var onError = function (err) {
-  console.log('An error occurred:', gutil.colors.magenta(err.message));
+var onError = function(err){
+  console.log('An error occurred:', gutil.colors.yellow(err.message));
   gutil.beep();
   this.emit('end');
-};
+},
+reload = browserSync.reload;
 
 // Move Bootstrap CSS to dist/css
-gulp.task('boots', function(){
+gulp.task('boots', ['bootsjs'], function(){
   return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
     .pipe(gulp.dest("./dist/css"));
 });
 
+// Move JS Files to src/js
+gulp.task('bootsjs', function(){
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/popper.min.js'])
+    .pipe(gulp.dest("./src/js"))
+});
+
 // Move Fonts Folder to /fonts
-gulp.task('fonts', function(){
+gulp.task('fonts', ['fa'], function(){
   return gulp.src('node_modules/font-awesome/fonts/*')
     .pipe(gulp.dest("./fonts"));
 });
@@ -42,11 +50,11 @@ gulp.task('fa', function(){
 // Move particles.js to src/js
 gulp.task('particles', function(){
   return gulp.src('node_modules/particles.js/particles.js')
-    .pipe(gulp.dest("./src/js"));
+    .pipe(gulp.dest("/src/js"));
 });
 
 // Move Lightbox js to src/js
-gulp.task('ekkojs', function(){
+gulp.task('ekkojs', ['ekkocss'], function(){
   return gulp.src('node_modules/ekko-lightbox/dist/ekko-lightbox.min.js')
     .pipe(gulp.dest("./src/js"));
 });
@@ -57,45 +65,70 @@ gulp.task('ekkocss', function(){
     .pipe(gulp.dest("./dist/css"));
 });
 
-// Move JS Files to src/js
-gulp.task('js', function(){
-  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js','node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest("./src/js"))
+// Concat and minify js files and move to /dist/js
+gulp.task('js', function() {
+  return gulp.src('./src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('app.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
     .pipe(browserSync.stream());
 });
+
+// Optimize Images and move to /dist/images
+gulp.task('imageMin', function(){
+  gulp.src('./src/img/*')
+  .pipe(plumber({errorHandler: onError}))
+  .pipe(cache(imageMin({
+     optimizationLevel: 5,
+     progressive: true,
+     interlaced: true
+  })))
+     .pipe(gulp.dest('./images'))
+     .pipe(browserSync.stream());
+  });
+
+  // Remove unused css
+  gulp.task('purify', function() {
+    return gulp.src('src/*.css')
+      .pipe(purify(['src/*.js', 'src/*.html']))
+      .pipe(gulp.dest('./dist/'));
+  });
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
   return gulp.src('./sass/**/*.scss')
    .pipe(plumber({ errorHandler: onError }))
-  .pipe(sass())
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('./'))
+   .pipe(sass({
+     indentType: 'tab',
+     indentWidth: 1,
+     outputStyle: 'expanded',
+   }))
+   .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      })
+    ]))
+    .pipe(groupmq())
+    .pipe(gulp.dest('./'))
+    .pipe(rtlcss())
+    .pipe(rename({ basename: 'rtl' }))
+    .pipe(gulp.dest('./'));
+    });
 
-  .pipe(rtlcss())
-  .pipe(rename({ basename: 'rtl' }))
-  .pipe(gulp.dest('./'));
-});
+ // Watch Sass & Server
+ gulp.task('serve', function(){
+   browserSync.init({
+     files: ['./**/*.php'],
+     proxy: 'http://localhost/'
+   });
 
-gulp.task('watch', function(){
-  browserSync.init({
-    files: ['./**/*.php'],
-    proxy: 'http://localhost',
-  });
-  gulp.watch('./sass/**/*.scss', ['sass'])
-  gulp.watch('./js/*.js', ['js', reload]);
-  gulp.watch('images/src/*', ['images', reload]);
+   gulp.watch(['./sass/**/*.scss'], ['sass']).on('change', reload);
+   gulp.watch(['./src/js/*.js'], ['js']).on('change', reload);
+   gulp.watch(['images/src/*'], ['imageMin']).on('change', reload);
  });
 
-// Optimize Images and move to /dist/images
-gulp.task('imageMin', () =>
-   gulp.src('src/images/*')
-   .pipe(cache(imagemin({
-      optimizationLevel: 5,
-      progressive: true,
-      interlaced: true
-   })))
-   .pipe(gulp.dest('dist/images'))
-);
-
-gulp.task('default', ['sass', 'watch']);
+gulp.task('start', ['boots', 'fonts']);
